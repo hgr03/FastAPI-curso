dff91d95a864ea5091c1f8bec55411bfaa2efb51
Hector G <hgr.0326@gmail.com>
2024-06-14 16:27:56 -0600

    se agregan parametros de consulta y ruta

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#regresa un mensaje
 @app.get('/')
 async def read_root():
     return {"message": "Hello world"}
+
+#Parametros por ruta /1000
+@app.get('/item/{item_id}')
+async def read_item(item_id:int , q:Union[str, None] = None):
+    return {'item_id':item_id, 'q':q}
+
+
+#parametros por consulta ?num_1=1&num_2=2
+@app.get('/calculadora')
+async def calculator(num_1:float, num_2:float):
+    return {"suma": num_1+num_2}
